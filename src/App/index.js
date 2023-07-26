0935d27f4335814af57ea04b7bc6971fb950005b
Nico Mendoza <nicomendoza274@gmail.com>
2023-07-26 08:11:20 -0300

    demo useEffect

--- src/App/index.js
+++ src/App/index.js
@@ -21,6 +21,15 @@ function App() {
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
 
+  console.log('Log 1');
+  // React.useEffect(() => {
+  //   console.log('Looooooog 2');
+  // })
+  React.useEffect(() => {
+    console.log('Looooooog 2');
+  }, [totalTodos])
+  console.log('Log 3');
+
   const searchedTodos = todos.filter(todo => {
     const todoText = todo.text.toLowerCase();
     const searchText = searchValue.toLowerCase();
