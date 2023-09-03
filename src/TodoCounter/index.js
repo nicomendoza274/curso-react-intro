c5e79405dba5d96efce433ab5b3480bf2c7785e3
Nico Mendoza <nicomendoza274@gmail.com>
2023-09-03 17:48:22 -0300

    Refactor loading

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './TodoCounter.css';
 
-function TodoCounter({ completedTodos,totalTodos}) {
+function TodoCounter({ completedTodos,totalTodos, loading}) {
 
     return (
-        <h1 className='TodoCounter'>
+        <h1 className={`TodoCounter ${!!loading && 'TodoCounter--loading'}`}>
             Has compeltado <span>{completedTodos}</span> de <span>{totalTodos}</span> TODOS
         </h1>
     );
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch({ searchValue, setSearchValue }) {
+function TodoSearch({ searchValue, setSearchValue, loading }) {
 
     return (
         <input 
@@ -12,6 +12,7 @@ function TodoSearch({ searchValue, setSearchValue }) {
                 const value = event.target.value
                 setSearchValue(value)
             }}
+            disabled={loading}
         />
     );
 }
